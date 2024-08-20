aff55af4c6a91e9c116ec497cb3c1b5b262ac64a
Lisa Koekemoer <alicia.koekemoer98@gmail.com>
2024-08-20 18:37:50 +0200

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.svg';
+import document from './images/icon-document.svg';
+import folder from './images/icon-folder.svg';
+import upload from './images/icon-upload.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='storage-component'>
+
+        {/* Fylo-Functions Component */}
+        <div className='fylo-functions'>
+          <img className='logo' src={logo} alt="" />
+          <div className='functions'>
+            <div className='functions-comp'>
+              <img src={document} alt="" />
+            </div>
+            <div className='functions-comp'>
+              <img src={folder} alt="" />
+            </div>
+            <div className='functions-comp'>
+              <img src={upload} alt="" />
+            </div>
+          </div>
+        </div>
+
+        {/* Storage Indicator Component */}
+        <div className='storage'>
+          <div className='storage-info'>
+            <p className='storage-indication'>You've used <span>815 GB</span> of your storage</p>
+            <div className='full-usage'>
+              <div className='used-storage'>
+                <div className='indicator'>
+                </div>
+              </div>
+            </div>
+            <div className='parameters'>
+              <p>0 GB</p>
+              <p>1000 GB</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Remaining Storage Component */}
+        <div className='bubble'>
+          <h1>1<span>8</span>5</h1>
+          <p>GB left</p>
+        </div>
+      </div>
+    </>
   );
 }
 
